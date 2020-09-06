e2fd6b876dc6a166a1f66b5625257d61570721a0
Tomba <tom@tommelloul.com>
2020-09-06 22:33:42 +0300

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -4,46 +4,20 @@ var repo = pathname.replace(/\//g, '');
 var user = hostname.split(".")[0];
 var domain = hostname.split(".")[1];
 
-var pathname = window.location.pathname;
-var hostname = window.location.hostname;
-var repo = pathname.replace(/\//g, '');
-var user = hostname.split(".")[0];
-var domain = hostname.split(".")[1];
-
-
 var galleryContainer = $("#projectsContent > .gallery.container");
-var project = $("#projectGallery"); 
 var oldid, slider;
 
-
 $(document).ready(function () {
+	var folder = "../../images/portfolio/";
+	var ajaxUrl;
 	if (domain == 'github') {
-		firstGit();
+		ajaxUrl = "contents.html?/images/portfolio/";
 	}
 	else {
-		firstAjax();
+		ajaxUrl = folder;
 	}
-});
-
-function showProject(id) {
-	if (oldid != id) {
-		galleryContainer.empty(); 
-		$('<ul></ul>').appendTo(galleryContainer).attr('id', 'projectGallery').ready(function() {
-			if (domain == 'github') {
-				startGit(id);
-			}
-			else {
-				startAjax(id);
-			}
-		});
-	}
-}
-
-
-function firstAjax() {
-	var folder = "../../images/portfolio/";	
 	$.ajax({
-		url: folder,
+		url: ajaxUrl,
 		success: function (data) {
 			$(data).find("a").attr("href", function (i, val) {
 				if (val.match(/\.(jpe?g|png|gif|webp)$/)) {
@@ -70,197 +44,29 @@ function firstAjax() {
 		}
 	});
 	$(".project").css("background-size", "cover");
-}
+});
 
-function firstGit() {
-	(async () => {
-		const response = await fetch('https://api.github.com/repos/' + user + '/' + repo + '/contents/images/portfolio');
-		const data = await response.json();
-		for (let file of data) {
-			var val = file.name;
-			var url = '/' + repo + '/' + file.path;
-			if (val.match(/\.(jpe?g|png|gif|webp)$/)) {
-				var id = val.split('.').slice(0, -1).join('.');
-				var caption = id.replace('%20', "\n");
-				var descTitle = "";
-				var desc = "";
-				$.ajax({
-					url: url.split('.').slice(0, -1).join('.') + ".txt",
-					async: false,
-					cache: false,
-					success: function (txt) {
-						var lines = txt.split("\n");
-						caption = lines[0].replace(' ', "\n");
-						descTitle = lines[1];
-						desc = lines[2];
-					}
-				});
-				$("#portfolioGallery").append('<a href="#projects/' + id.substring(id.indexOf('%20') + 4) + '"><div class="project" id="' + id + '" title="' + caption + '" data-descTitle="' + descTitle + '" data-desc="' + desc + '" style="background-image: url(&apos;' + url + '&apos;)"></div></a>');
-			}
-		}
-	})()
-}
-
-function startGit(id) {
-	(async () => {
-		const response = await fetch('https://api.github.com/repos/' + user + '/' + repo + '/contents/projects/' + id);
-		const data = await response.json();
-		for (let file of data) {
-			var val = file.name;
-			var folder = url.split('/').pop() + '/';
-			if (val.match(/\.(jpe?g|png|gif|webp)$/)) {
-				var id = val.split('.').slice(0, -1).join('.');
-				var url = folder + val;
-				var thumbUrl = folder + 'thumb/' + id + '.webp';
-				var caption = id;
-				var descTitle;
-				var desc;
-
-				$.ajax({
-					url: folder + id + ".txt",
-					async: false,
-					cache: false,
-					success: function (txt) {
-						var lines = txt.split("\n");
-						caption = lines[0].replace(' ', "\n");
-						descTitle = lines[1];
-						desc = lines[2];
-					},
-					complete: function (data) {
-						$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"> <img src="' + url + '" /> </li>').appendTo(project);
-					}
-				});
-			}
-			if (val.match(/\.(htm|html)$/)) {
-				var id = val.split('.').slice(0, -1).join('.');
-				var url = folder + val;
-				var thumbUrl = folder + 'thumb/' + id + '.webp';
-				var caption = id.replace('%20', "\n");
-				var descTitle;
-				var desc;
-				var html;
-
-				$.get(url, function (html_string) {
-					html = html_string;
-				}, 'html');
-				$.ajax({
-					url: folder + id + ".txt",
-					async: false,
-					cache: false,
-					success: function (txt) {
-						var lines = txt.split("\n");
-						caption = lines[0].replace(' ', "\n");
-						descTitle = lines[1];
-						desc = lines[2];
-					},
-					complete: function (data) {
-						$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"></li>').appendTo(project).attr('id', id + '-HTML').ready(function () {
-							$(function () {
-								window.setTimeout(function () {
-									$('#' + id + '-HTML').append(html);
-								}, 100);
-							});
-						});
-					}
-				});
-			}
-			if (val.match(/\.(url)$/)) {
-				var id = val.split('.').slice(0, -1).join('.');
-				var url = folder + val;
-				var thumbUrl = folder + 'thumb/' + id + '.webp';
-				var caption = id.replace('%20', "\n");
-				var descTitle;
-				var desc;
-				var html;
-
-				$.get(url, function (html_string) {
-					html = html_string;
-				}, 'html');
-				$.ajax({
-					url: folder + id + ".txt",
-					async: false,
-					cache: false,
-					success: function (txt) {
-						var lines = txt.split("\n");
-						caption = lines[0].replace(' ', "\n");
-						descTitle = lines[1];
-						desc = lines[2];
-					},
-					complete: function (data) {
-						$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"></li>').appendTo(project).attr('id', id + '-VID').ready(function () {
-							$(function () {
-								$('#' + id + '-VID').append('<iframe src="' + html.split('URL=')[1] + '?quality=720p&byline=0&portrait=0&title=0autoplay=1" width="100%" height="100%" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>');
-							});
-						});
-					}
-				});
-			}
-			if (val.match(/\.(pdf)$/)) {
-				var id = val.split('.').slice(0, -1).join('.');
-				var url = folder + val;
-				var thumbUrl = folder + 'thumb/' + id + '.webp';
-				var caption = id.replace('%20', "\n");
-				var descTitle;
-				var desc;
-
-				$.ajax({
-					url: folder + id + ".txt",
-					async: false,
-					cache: false,
-					success: function (txt) {
-						var lines = txt.split("\n");
-						caption = lines[0].replace(' ', "\n");
-						descTitle = lines[1];
-						desc = lines[2];
-					},
-					complete: function (data) {
-						$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"><div id="' + id + '-PDF" style="height:100%;"></div></li>').appendTo(project).ready(function () {
-							$(function () {
-								$('#' + id + '-PDF').pdf({
-									source: url,
-									title: id + " Booklet"
-								});
-							});
-						});
-					}
-				});
-			}
-		}
-		window.setTimeout(function () {
-			project.lightSlider({
-				gallery: true,
-				item: 1,
-				loop: false,
-				auto: false,
-				slideMargin: 0,
-				galleryMargin: 10,
-				thumbItem: 9,
-				currentPagerPosition: 'middle',
-				onSliderLoad: function () {
-					$('ul.lSPager, ul.lSPager li').removeAttr('style');
-				},
-				onBeforeSlide: function () {
-					var iframe = project.find('li.lslide.active iframe');
-					if (iframe[0] !== undefined) {
-						var src = iframe.attr('src');
-						if (src.indexOf('vimeo') > -1) {
-							var player = new Vimeo.Player(iframe[0]);
-							player.pause();
-						}
-					}
-
-				},
-				onAfterSlide: function () {}
-			});
-			oldid = id;
-		}, 100);
-	})()
+function showProject(id) {
+	if (oldid != id) {
+		galleryContainer.empty(); 
+		$('<ul></ul>').appendTo(galleryContainer).attr('id', 'projectGallery').ready(function() {
+			startAjax(id);
+		});
+	}
 }
 
 function startAjax(id) {
+	var project = $("#projectGallery"); 
 	var folder = "../../projects/" + id + "/";
+	var ajaxUrl;
+	if (domain == 'github') {
+		ajaxUrl = "contents.html?/projects/" + id + "/";
+	}
+	else {
+		ajaxUrl = folder;
+	}
 	$.ajax({
-		url: folder,
+		url: ajaxUrl,
 		success: function (data) {
 			$(data).find("a").attr("href", function (i, val) {
 				if (val.match(/\.(jpe?g|png|gif|webp)$/)) {
@@ -283,39 +89,6 @@ function startAjax(id) {
 						},
 						complete: function (data) {
 							$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"> <img src="' + url + '" /> </li>').appendTo(project);
-						}
-					});
-				}
-				if (val.match(/\.(htm|html)$/)) {
-					var id = val.split('.').slice(0, -1).join('.');
-					var url = folder + val;
-					var thumbUrl = folder + 'thumb/' + id + '.webp';
-					var caption = id.replace('%20', "\n");
-					var descTitle;
-					var desc;
-					var html;
-					
-					$.get(url, function (html_string) {
-						html = html_string;
-					}, 'html');
-					$.ajax({
-						url: folder + id + ".txt",
-						async: false,
-						cache: false,
-						success: function (txt) {
-							var lines = txt.split("\n");
-							caption = lines[0].replace(' ', "\n");
-							descTitle = lines[1];
-							desc = lines[2];
-						},
-						complete: function (data) {
-							$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"></li>').appendTo(project).attr('id', id + '-HTML').ready(function() {
-								$(function() {
-									window.setTimeout(function () {
-										$('#' + id + '-HTML').append(html);
-									}, 100);						
-								});
-							});
 						}
 					});
 				}
@@ -380,35 +153,35 @@ function startAjax(id) {
 			});
 		},
 		complete: function (data) {
-			window.setTimeout(function () {
-				project.lightSlider({
-					gallery: true,
-					item: 1,
-					loop: false,
-					auto: false,
-					slideMargin: 0,
-					galleryMargin: 10,
-					thumbItem: 9,
-					currentPagerPosition: 'middle',
-					onSliderLoad: function() {
-						$('ul.lSPager, ul.lSPager li').removeAttr('style');
-					},
-					onBeforeSlide: function () {
-						var iframe = project.find('li.lslide.active iframe');
-						if (iframe[0] !== undefined) {
-							var src = iframe.attr('src');
-							if (src.indexOf('vimeo') > -1) {
-								var player = new Vimeo.Player(iframe[0]);
-								player.pause();
-							}
-						}
-
-					},
-					onAfterSlide: function () {
+			project.lightSlider({
+				gallery: true,
+				item: 1,
+				loop: false,
+				auto: false,
+				slideMargin: 0,
+				galleryMargin: 10,
+				thumbItem: 9,
+				currentPagerPosition: 'middle',
+				onSliderLoad: function() {
+					$('ul.lSPager, ul.lSPager li').removeAttr('style');
+				},
+				onBeforeSlide: function () {
+					var iframe = project.find('li.lslide.active iframe')[0];
+					if (iframe !== undefined) {
+						var player = new Vimeo.Player(iframe);
+						player.pause();
 					}
-				});
-				oldid = id;
-			}, 100);
+					
+				},
+				onAfterSlide: function () {
+					var iframe = project.find('li.lslide.active iframe')[0];
+/*					if (iframe !== undefined) {
+						var player = new Vimeo.Player(iframe);
+						player.play();
+					}*/
+				}
+			});
+			oldid = id;
 		}
 	});
 
