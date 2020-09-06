2cc8c1fbfc87c379f38def203f78f046a1517da5
Tomba <tom@tommelloul.com>
2020-09-06 15:34:06 +0300

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -127,7 +127,7 @@ function showProject(id) {
 
 function firstGit() {
 	(async () => {
-		const response = await fetch('https://api.github.com/repos/' + user + '/' + repo + '/contents/');
+		const response = await fetch('https://api.github.com/repos/' + user + '/' + repo + '/contents/projects/');
 		const data = await response.json();
 		for (let file of data) {
 			var val = file.name;
