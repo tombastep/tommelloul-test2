047115253e8503e88d555791d4ebeda1fab5c9d3
Tomba <tom@tommelloul.com>
2020-09-06 15:25:01 +0300

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,7 +1,3 @@
-var folder = "../../images/portfolio/";
-var galleryContainer = $("#projectsContent > .gallery.container");
-var oldid, slider;
-
 $.fn.exists = function(callback) {
   var args = [].slice.call(arguments, 1);
 
@@ -95,10 +91,71 @@ function ResizeSensor(element, callback)
 };
 
 
+var pathname = window.location.pathname;
+var hostname = window.location.hostname;
+var repo = pathname.split("/")[1];
+var user = hostname.split(".")[0];
+var domain = hostname.split(".")[1];
 
+var galleryContainer = $("#projectsContent > .gallery.container");
+var oldid, slider;
 
 
 $(document).ready(function () {
+	if (domain == 'github') {
+		firstGit();
+	}
+	else {
+		firstAjax();
+	}
+});
+
+function showProject(id) {
+	if (oldid != id) {
+		galleryContainer.empty(); 
+		$('<ul></ul>').appendTo(galleryContainer).attr('id', 'projectGallery').ready(function() {
+			if (domain == 'github') {
+//				startGit(id);
+				alert('git');
+			}
+			else {
+				startAjax(id);
+			}
+		});
+	}
+}
+
+function firstGit() {
+	(async () => {
+		const response = await fetch('https://api.github.com/repos/' + user + '/' + repo + '/contents/');
+		const data = await response.json();
+		for (let file of data) {
+			var val = file.name;
+			var url = file.path;
+			if (val.match(/\.(jpe?g|png|gif|webp)$/)) {
+				var id = val.split('.').slice(0, -1).join('.');
+				var caption = id.replace('%20', "\n");
+				var descTitle = "";
+				var desc = "";
+				$.ajax({
+					url: url.split('.').slice(0, -1).join('.') + ".txt",
+					async: false,
+					cache: false,
+					success: function (txt) {
+						var lines = txt.split("\n");
+						caption = lines[0].replace(' ', "\n");
+						descTitle = lines[1];
+						desc = lines[2];
+					}
+				});
+				$("#portfolioGallery").append('<a href="#projects/' + id.substring(id.indexOf('%20') + 3) + '"><div class="project" id="' + id + '" title="' + caption + '" data-descTitle="' + descTitle + '" data-desc="' + desc + '" style="background-image: url(&apos;' + url + '&apos;)"></div></a>');
+			}
+		}
+	})()
+}
+
+function firstAjax() {
+	var folder = "../../images/portfolio/";	
 	$.ajax({
 		url: folder,
 		success: function (data) {
@@ -127,15 +184,6 @@ $(document).ready(function () {
 		}
 	});
 	$(".project").css("background-size", "cover");
-});
-
-function showProject(id) {
-	if (oldid != id) {
-		galleryContainer.empty(); 
-		$('<ul></ul>').appendTo(galleryContainer).attr('id', 'projectGallery').ready(function() {
-			startAjax(id);
-		});
-	}
 }
 
 function startAjax(id) {
@@ -165,6 +213,39 @@ function startAjax(id) {
 						},
 						complete: function (data) {
 							$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"> <img src="' + url + '" /> </li>').appendTo(project);
+						}
+					});
+				}
+				if (val.match(/\.(htm|html)$/)) {
+					var id = val.split('.').slice(0, -1).join('.');
+					var url = folder + val;
+					var thumbUrl = folder + 'thumb/' + id + '.webp';
+					var caption = id.replace('%20', "\n");
+					var descTitle;
+					var desc;
+					var html;
+					
+					$.get(url, function (html_string) {
+						html = html_string;
+					}, 'html');
+					$.ajax({
+						url: folder + id + ".txt",
+						async: false,
+						cache: false,
+						success: function (txt) {
+							var lines = txt.split("\n");
+							caption = lines[0].replace(' ', "\n");
+							descTitle = lines[1];
+							desc = lines[2];
+						},
+						complete: function (data) {
+							$('<li title="' + caption + '" data-thumb="' + thumbUrl + '"></li>').appendTo(project).attr('id', id + '-HTML').ready(function() {
+								$(function() {
+									window.setTimeout(function () {
+										$('#' + id + '-HTML').append(html);
+									}, 100);						
+								});
+							});
 						}
 					});
 				}
@@ -229,35 +310,35 @@ function startAjax(id) {
 			});
 		},
 		complete: function (data) {
-			project.lightSlider({
-				gallery: true,
-				item: 1,
-				loop: false,
-				auto: false,
-				slideMargin: 0,
-				galleryMargin: 10,
-				thumbItem: 9,
-				currentPagerPosition: 'middle',
-				onSliderLoad: function() {
-					$('ul.lSPager, ul.lSPager li').removeAttr('style');
-				},
-				onBeforeSlide: function () {
-					var iframe = project.find('li.lslide.active iframe')[0];
-					if (iframe !== undefined) {
-						var player = new Vimeo.Player(iframe);
-						player.pause();
+			window.setTimeout(function () {
+				project.lightSlider({
+					gallery: true,
+					item: 1,
+					loop: false,
+					auto: false,
+					slideMargin: 0,
+					galleryMargin: 10,
+					thumbItem: 9,
+					currentPagerPosition: 'middle',
+					onSliderLoad: function() {
+						$('ul.lSPager, ul.lSPager li').removeAttr('style');
+					},
+					onBeforeSlide: function () {
+						var iframe = project.find('li.lslide.active iframe');
+						if (iframe[0] !== undefined) {
+							var src = iframe.attr('src');
+							if (src.indexOf('vimeo') > -1) {
+								var player = new Vimeo.Player(iframe[0]);
+								player.pause();
+							}
+						}
+
+					},
+					onAfterSlide: function () {
 					}
-					
-				},
-				onAfterSlide: function () {
-					var iframe = project.find('li.lslide.active iframe')[0];
-/*					if (iframe !== undefined) {
-						var player = new Vimeo.Player(iframe);
-						player.play();
-					}*/
-				}
-			});
-			oldid = id;
+				});
+				oldid = id;
+			}, 100);
 		}
 	});
 
